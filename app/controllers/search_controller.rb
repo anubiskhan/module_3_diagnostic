f5cc9063c345ebe7f14a0553858d4bd061a404ef
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-16 10:15:41 -0600

    working on making a station PORO to handle the API return for easier view output

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,43 @@
 class SearchController < ApplicationController
   def index
-    @elec = Faraday.get("https://developer.nrel.gov//api/alt-fuel-stations/v1/nearest.json") do |req|
+    @resp = Faraday.get("https://developer.nrel.gov//api/alt-fuel-stations/v1/nearest.json") do |req|
       req.headers['X-Api-Key'] = ENV['DEV_NET_API_KEY']
       req.params['location'] = "#{params[:q]}"
-      req.params['fuel_type'] = "ELEC"
+      req.params['fuel_type'] = "ELEC,LPG"
       req.params['limit'] = "10"
     end
-    @lpg = Faraday.get("https://developer.nrel.gov//api/alt-fuel-stations/v1/nearest.json") do |req|
-      req.headers['X-Api-Key'] = ENV['DEV_NET_API_KEY']
-      req.params['location'] = "#{params[:q]}"
-      req.params['fuel_type'] = "LPG"
-      req.params['limit'] = "10"
+    @body = JSON.parse(@resp.body)
+    @stations = @body['fuel_stations']
+    @stations.each do |station|
+      Station.new(station)
     end
-    @body1 = JSON.parse(@elec.body)
-    @body2 = JSON.parse(@resp.body)
     binding.pry
   end
+
+  class Station
+    def initialize(station_hash)
+      @station_hash = station_hash
+      byebug
+    end
+    def name
+      @station_hash['station_name']
+    end
+
+    def address
+      "#{@station_hash['street_address']} #{@station_hash['city']}, #{@station_hash['state']} #{@station_hash['zip']}"
+    end
+
+    def fuel_types
+      @station_hash['fuel_type_code']
+    end
+
+    def distance
+
+    end
+
+    def access_times
+
+    end
+  end
 end
+# Name, Address, Fuel Types, Distance, and Access Times
