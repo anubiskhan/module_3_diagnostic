cc51131e57a9c613324bf8cce595b527c6dfcc51
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-16 09:58:23 -0600

    Working with params in order to return only ELEC and LPG stations

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,19 @@
 class SearchController < ApplicationController
   def index
-    @resp = Faraday.get("https://developer.nrel.gov//api/alt-fuel-stations/v1/nearest.json") do |req|
+    @elec = Faraday.get("https://developer.nrel.gov//api/alt-fuel-stations/v1/nearest.json") do |req|
       req.headers['X-Api-Key'] = ENV['DEV_NET_API_KEY']
       req.params['location'] = "#{params[:q]}"
+      req.params['fuel_type'] = "ELEC"
+      req.params['limit'] = "10"
     end
-    @body = JSON.parse(@resp.body)
+    @lpg = Faraday.get("https://developer.nrel.gov//api/alt-fuel-stations/v1/nearest.json") do |req|
+      req.headers['X-Api-Key'] = ENV['DEV_NET_API_KEY']
+      req.params['location'] = "#{params[:q]}"
+      req.params['fuel_type'] = "LPG"
+      req.params['limit'] = "10"
+    end
+    @body1 = JSON.parse(@elec.body)
+    @body2 = JSON.parse(@resp.body)
+    binding.pry
   end
 end
