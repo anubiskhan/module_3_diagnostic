46d7783dc54bb30237920d6b85020fd487fe010d
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-05-16 10:57:36 -0600

    Adds distance testing for the stations model returns from the api

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -15,7 +15,23 @@ describe Station do
     end
 
     it 'has stations presorted by distance via the API' do
+      resp = File.read('./spec/api_return.json')
 
+      # stations_list = []
+      # JSON.parse(resp)['fuel_stations'].each do |s|
+      #   stations_list.push(Station)
+      # end
+      list = JSON.parse(resp)['fuel_stations']
+
+      expect(list[0]['distance']).to be < list[1]['distance']
+      expect(list[1]['distance']).to be < list[2]['distance']
+      expect(list[2]['distance']).to be < list[3]['distance']
+      expect(list[3]['distance']).to be < list[4]['distance']
+      expect(list[4]['distance']).to be < list[5]['distance']
+      expect(list[5]['distance']).to be < list[6]['distance']
+      expect(list[6]['distance']).to be < list[7]['distance']
+      expect(list[7]['distance']).to be < list[8]['distance']
+      expect(list[8]['distance']).to be < list[9]['distance']
     end
   end
 end
